97171ae53635d9784ae988fe570f65154e1d57eb
Evan <calliaevan12@gmail.com>
2016-12-07 08:57:16 -0800

    updated sql connection

--- Factories/CommentFactory.cs
+++ Factories/CommentFactory.cs
@@ -14,7 +14,7 @@ namespace theWall.Factory
         public CommentRepository()
         {
             // connectionString = "server=localhost;userid=root;password=root;port=8889;database=thewalldb;SslMode=None";
-            connectionString = "server=localhost;userid=root;password=root;port=3306;database=thewalldb;SslMode=None";            
+            connectionString = "server=localhost;userid=remote;password=password;port=3306;database=thewalldb;SslMode=None";            
         }
 
         internal IDbConnection Connection
